a9bbb7bbc91a11712cff26addf20962dc3184a7d
Shizun Ge <shizunge@gmail.com>
2025-01-03 09:41:55 -0800

    Update copyright date

--- examples/webhook/run_gantry.sh
+++ examples/webhook/run_gantry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2024 Shizun Ge
+# Copyright (C) 2024-2025 Shizun Ge
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
