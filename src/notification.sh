ed926b157f067a3df429544e2ca3c79271fe3f8a
Shizun Ge <shizunge@gmail.com>
2026-01-05 20:05:16 -0800

    Update copyright to 2026.

--- src/notification.sh
+++ src/notification.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2023-2025 Shizun Ge
+# Copyright (C) 2023-2026 Shizun Ge
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- tests/spec_gantry_test_helper.sh
+++ tests/spec_gantry_test_helper.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=sh
-# Copyright (C) 2024-2025 Shizun Ge
+# Copyright (C) 2024-2026 Shizun Ge
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
