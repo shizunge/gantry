34572db9193ecb0b4e3fc12fd1a92d15b66999ae
Shizun Ge <shizunge@gmail.com>
2026-03-19 21:03:54 -0700

    [docs] Add documents about GANTRY_TRIGGER_PATH. Update webhook example.

--- examples/webhook/README.md
+++ examples/webhook/README.md
@@ -4,11 +4,11 @@ This example describes how to launch [*Gantry*](https://github.com/shizunge/gant
 
 ## Setup
 
-We leverage a dockerized webhook image [lwlook/webhook](https://hub.docker.com/r/lwlook/webhook) which is based on the offical Docker image. This allows us to launch the *Gantry* service with simple docker commands.
+We start two containers, *Gantry* and [lwlook/webhook](https://hub.docker.com/r/lwlook/webhook) that is a dockerized *adnanh/webhook* image. They communicate via a shared volume. *Gantry* watches a file in the shared volume, while *webhook* changes that file to trigger an update.
 
 [hooks.json](./hooks.json) defines the webhook's behavior. It parses incoming payloads and transforms them into environment variables like `GANTRY_SERVICES_EXCLUDED`, `GANTRY_SERVICES_EXCLUDED_FILTERS` and `GANTRY_SERVICES_FILTERS`. These variables are then used by [run_gantry.sh](./run_gantry.sh) to control *Gantry* behaviors. which means you can update different services by passing different payloads to the webhook. Refer to the [adnanh/webhook](https://github.com/adnanh/webhook) repository for more advanced webhook configurations, including securing the webhook with `trigger-rule`.
 
-[run_gantry.sh](./run_gantry.sh) is responsible for launching the *Gantry* service.
+[run_gantry.sh](./run_gantry.sh) is responsible for updating `GANTRY_TRIGGER_PATH` that *Gantry* is watching.
 
 ## Test
 
@@ -21,13 +21,14 @@ docker stack deploy --detach=true --prune --with-registry-auth --compose-file ./
 Use `curl` to send a `POST` request to the webhook endpoint. This request tells the *Gantry* to only update the service named *webhook_webhook*.
 
 ```
-curl -X POST localhost:9000/hooks/run-gantry -H "Content-Type: application/json"  -d '{"GANTRY_SERVICES_FILTERS":"name=webhook_webhook"}'
+curl -v -X POST localhost:9000/hooks/run-gantry -H "Content-Type: application/json"  -d '{"GANTRY_SERVICES_FILTERS":"name=webhook_webhook"}'
 ```
 
-Check the webhook service logs to confirm if the webhook was triggered correctly.
+Check the service logs to confirm if the webhook and update was triggered correctly.
 
 ```
 docker service logs webhook_webhook
+docker service logs webhook_gantry
 ```
 
 Use the following command to stop the stack.
--- examples/webhook/run_gantry.sh
+++ examples/webhook/run_gantry.sh
@@ -15,80 +15,35 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-_docker_start_replicated_job() {
-  local args="${*}"
-  if [ -z "${args}" ]; then
-    echo "No services set."
-    echo "Services that are not running:"
-    docker service ls | grep "0/"
-    return 0
-  fi
-  for S in ${args}; do
-    echo -n "Set replicas to 0 to ${S}: "
-    docker service update --replicas=0 "${S}"
-    echo -n "Set replicas to 1 to ${S}: "
-    docker service update --detach --replicas=1 "${S}"
-  done
-}
-
-_get_number_of_running_tasks() {
-  local filter="${1}"
-  local replicas=
-  if ! replicas=$(docker service ls --filter "${filter}" --format '{{.Replicas}}' | head -n 1); then
-    return 1
-  fi
-  # https://docs.docker.com/engine/reference/commandline/service_ls/#examples
-  # The REPLICAS is like "5/5" or "1/1 (3/5 completed)"
-  # Get the number before the first "/".
-  local num_runs=
-  num_runs=$(echo "${replicas}/" | cut -d '/' -f 1)
-  echo "${num_runs}"
-}
-
-resume_gantry() {
-  local filter="label=webhook.run-gantry=true"
-  local service_name=
-  service_name=$(docker service ls --filter "${filter}" --format "{{.Name}}" | head -n 1)
-  if [ -z "${service_name}" ]; then
-    echo "Cannot find a service from ${filter}."
-    return 1
-  fi
-  local replicas=
-  if ! replicas=$(_get_number_of_running_tasks "${filter}"); then
-    echo "Failed to obtain task states of service from ${filter}."
-    return 1
-  fi
-  if [ "${replicas}" != "0" ]; then
-    echo "${service_name} is still running. There are ${replicas} running tasks."
-    return 1
-  fi
-  docker service update --detach --env-add "GANTRY_SERVICES_EXCLUDED=${GANTRY_SERVICES_EXCLUDED:-}" "${service_name}"
-  docker service update --detach --env-add "GANTRY_SERVICES_EXCLUDED_FILTERS=${GANTRY_SERVICES_EXCLUDED_FILTERS:-}" "${service_name}"
-  docker service update --detach --env-add "GANTRY_SERVICES_FILTERS=${GANTRY_SERVICES_FILTERS:-}" "${service_name}"
-  _docker_start_replicated_job "${service_name}"
-}
-
-launch_new_gantry() {
-  local service_name=
-  service_name="gantry-$(date +%s)"
-  docker service create \
-    --name "${service_name}" \
-    --mode replicated-job \
-    --constraint "node.role==manager" \
-    --env "GANTRY_SERVICES_EXCLUDED=${GANTRY_SERVICES_EXCLUDED:-}" \
-    --env "GANTRY_SERVICES_EXCLUDED_FILTERS=${GANTRY_SERVICES_EXCLUDED_FILTERS:-}" \
-    --env "GANTRY_SERVICES_FILTERS=${GANTRY_SERVICES_FILTERS:-}" \
-    --label "from-webhook=true" \
-    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-    shizunge/gantry
-  local return_value=$?
-  docker service logs --raw "${service_name}"
-  docker service rm "${service_name}"
-  return "${return_value}"
-}
-
 main() {
-  launch_new_gantry "${@}"
+  # Read environment variables
+  local TRIGGER_FILE="${TRIGGER_FILE:-}"
+  local UPDATE_START_FILE="${UPDATE_START:-}"
+  echo "TRIGGER_FILE=${TRIGGER_FILE}"
+  echo "UPDATE_START_FILE=${UPDATE_START_FILE}"
+  [ -z "${TRIGGER_FILE}" ] && return 1;
+  [ -z "${UPDATE_START_FILE}" ] && return 1;
+  # Perpare the trigger file to pass parameters to Gantry.
+  # We setup GANTRY_PRE_RUN_CMD of Gantry to source this file.
+  local TMP_FILE=
+  TMP_FILE=$(mktemp) || return 1;
+  echo "export GANTRY_SERVICES_EXCLUDED=${GANTRY_SERVICES_EXCLUDED:-}" > "${TMP_FILE}"
+  echo "export GANTRY_SERVICES_EXCLUDED_FILTERS=${GANTRY_SERVICES_EXCLUDED_FILTERS:-}" > "${TMP_FILE}"
+  echo "export GANTRY_SERVICES_FILTERS=${GANTRY_SERVICES_FILTERS:-}" > "${TMP_FILE}"
+  echo "echo \"environment variables loaded.\"" > "${TMP_FILE}"
+  local UPDATE_START_TIME=0
+  local TRIGGER_START_TIME=
+  TRIGGER_START_TIME=$(date +%s)
+  # Keep trying until it actually triggers the update.
+  while [ "${UPDATE_START_TIME}" -lt "${TRIGGER_START_TIME}" ]; do
+    echo "Triggering update."
+    cp "${TMP_FILE}" "${TRIGGER_FILE}"
+    sleep 1
+    # Check the update is actually triggered.
+    # We setup GANTRY_PRE_RUN_CMD of Gantry to update ${UPDATE_START_FILE}.
+    [ -f "${UPDATE_START_FILE}" ] && UPDATE_START_TIME=$(head -1 "${UPDATE_START_FILE}")
+  done
+  rm "${TMP_FILE}"
 }
 
 main "${@}"
